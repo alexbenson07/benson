4f6f242e24611f36ce30af2e1ba20eed2140a06b
Alex Benson <abenson@cobrapps.com>
2014-07-31 19:16:49 -0500

    initial structure commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+/**
+ * Created by alexbenson on 7/30/14.
+ */
+$(document).ready(function() {
+
+    $('#trigger-overlay, .overlay').click(function() {
+        $('.overlay').toggleClass("open");
+    });
+});
