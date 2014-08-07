e098aa9b6515631716c31998b25d5edcb596fb25
Alex Benson <abenson@cobrapps.com>
2014-08-07 00:01:35 -0500

    jquery working on i design bc

--- js/style.js
+++ js/style.js
@@ -1,3 +1,16 @@
 /**
  * Created by alexbenson on 8/6/14.
  */
+$( document ).ready(function() {
+    var list = ['It\'s Responsive', 'There\'s too much bad design out there', 'It\'s a challenge', 'I get to take an idea and make it a tangible product', 'Get to collaborate with people with different perspectives'];
+
+    var my_reason = function() {
+        return  list[Math.floor(Math.random() * list.length)];
+    }
+
+
+    $('.click').click(function() {
+        $('.results').text(my_reason());
+    });
+
+});
