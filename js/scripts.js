1c7a25c743383ab190241be0e2e0cd5a73692362
Alex Benson <abenson@cobrapps.com>
2014-08-09 21:06:24 -0500

    moved new style into index.html added href to project fixed height issues

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,15 @@
  */
 $(document).ready(function() {
 
+
     $('#trigger-overlay, .overlay').click(function() {
         $('.overlay').toggleClass("open");
+        console.log('overlay');
     });
+
+    var parentHeight = $('.parent').height();
+    var childHeight = $('.child').height();
+    $('#child').css('margin-top', (parentHeight - childHeight) / 2);
 });
+
+
