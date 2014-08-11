64bc049a217ea98197626b187165a1380e4594ee
Alex Benson <abenson@cobrapps.com>
2014-08-11 00:56:08 -0500

    finally got centering jquery to work i amm an idiot

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 
     var parentHeight = $('.parent').height();
     var childHeight = $('.child').height();
-    $('#child').css('margin-top', (parentHeight - childHeight) / 2);
+    $('.child').css('margin-top', (parentHeight - childHeight) / 2);
 });
 
 
