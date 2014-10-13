7a3ed6a6779e35229e5a4c7c0fed8913b8c4651a
Blake Tarter <blake@Blakes-MacBook-Pro.local>
2014-10-13 01:52:57 -0500

    almost finished with wp setup

--- scs/wp-content/themes/reverie-master/footer.php
+++ scs/wp-content/themes/reverie-master/footer.php
@@ -1,60 +1,162 @@
-	</div><!-- Row End -->
-</div><!-- Container End -->
-
-<div class="full-width footer-widget">
-	<div class="row">
-		<?php dynamic_sidebar("Footer"); ?>
-	</div>
-</div>
-
-<footer class="full-width" role="contentinfo">
-	<div class="row">
-		<div class="large-12 columns">
-			<?php wp_nav_menu(array('theme_location' => 'utility', 'container' => false, 'menu_class' => 'inline-list')); ?>
-		</div>
-	</div>
-	<div class="row love-reverie">
-		<div class="large-12 columns">
-			<p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. <?php _e('Made with Love in','reverie'); ?> <a href="http://themefortress.com/reverie/" rel="nofollow" title="Reverie Framework">Reverie</a>.</p>
-		</div>
-	</div>
+<footer class="section footer">
+	<h2 class="header contact">Contact</h2>
+	<h3 class="subheader">Email</h3>
+	<p class="email">
+		<a href="mailto:jcstartup@gmail.com">jcstartup@gmail.com</a>
+	</p>
+	<h3 class="subheader">Phone</h3>
+	<p class="phone">
+		<a href="tel:8162224444">816.222.4444</a>
+	</p>
+	<h3 class="subheader">Social</h3>
+	<ul class="social-list small-block-grid-4">
+		<li class="social-item"></li>
+		<li class="social-item"></li>
+		<li class="social-item"></li>
+		<li class="social-item"></li>
+	</ul>
 </footer>
 
 <?php wp_footer(); ?>
+<script src="<?php echo get_template_directory_uri(); ?>/js/skrollr.min.js"></script>
 
 <!-- <script type="text/javascript" src="app"></script> -->
 
-<script>
+<script type="text/javascript">
 	(function($) {
 		$(document).foundation();
-	    (function build_hero() {
-	        var hero = $('#hero-video'),
-		        video = document.getElementById('hero-video'),
-	            heroRatio = '16:9',
-	            win = $(window),
-	            winWidth = win.width();
 
-	            function playPause() {
-	            	if (video.paused == false) {
-				        video.pause();
-				        this.firstChild.nodeValue = 'Play';
-				        // pauseCount();
-				    } else {
-				        video.play();
-				        this.firstChild.nodeValue = 'Pause';
-				        // startCount();
-				    }
-	            }
+		var s = skrollr.init();
+		var fourPoint = $('.four-point');
 
-	            hero.on('click', function(e) {
-	            	playPause();
-	            });
+		function isScrolledIntoView(elem) {
+			var docViewTop = $(window).scrollTop();
+			var docViewBottom = docViewTop + $(window).height();
 
-	            document.getElementById('hero-title').addEventListener('click', playPause);
+			var elemTop = $(elem).offset().top;
+			var elemBottom = elemTop + $(elem).height();
 
-	            hero.width(winWidth)
-	            hero.height((winWidth * 9) / 16).css('margin-left', '-3.125rem');
-	    }());
+			return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+		}
+
+		(function build_nav(){
+			var nav     = $('.navbar');
+
+			$('.nav-item.logo').on('click', function(e) {
+				// console.log('test');
+
+				e.preventDefault();
+
+				$('html, body').animate({
+					scrollTop: 0
+				}, 'ease');
+			});
+
+			$('.nav-item.contact').on('click', function(e) {
+				e.preventDefault();
+
+				$('html, body').animate({
+					scrollTop: $(document).height()
+				}, 'ease');
+			});
+
+			$('.nav-item.pricing').on('click', function(e) {
+				e.preventDefault();
+
+				$('html, body').animate({
+					scrollTop: $('.pricing-wrapper').offset().top - 180
+				}, 'ease');
+			});
+
+			$('.nav-item.mission').on('click', function(e) {
+				e.preventDefault();
+
+				$('html, body').animate({
+					scrollTop: $('.section.second').offset().top - 180
+				}, 'ease');
+			});
+
+			$('.nav-item.careers').on('click', function(e) {
+				e.preventDefault();
+
+				$('html, body').animate({
+					scrollTop: $('.four-point-wrapper').offset().top - 180
+				}, 'ease');
+			});
+		}());
+
+		(function build_first_section(){
+			var first = $('.section.first');
+
+			first.children('.left').css('margin-top', ($(window).height() /2) - (first.children('.left').height() / 2));
+		}());
+
+		$(document).on('scroll', function() {
+			if (!fourPoint.hasClass('in-view')) {
+				if (isScrolledIntoView(fourPoint)) {
+					fourPoint.addClass('in-view');
+				}
+			}
+		});
+
+
+		(function build_video() {
+				var hero = $('#video'),
+						video = document.getElementById('video'),
+						heroRatio = '16:9',
+						win = $(window),
+						winWidth = win.width(),
+						wrapper = $('.video-wrapper');
+
+						// function playPause() {
+						//   if (video.paused == false) {
+						//       video.pause();
+						//       this.firstChild.nodeValue = 'Play';
+						//       // pauseCount();
+						//   } else {
+						//       video.play();
+						//       this.firstChild.nodeValue = 'Pause';
+						//       // startCount();
+						//   }
+						//     }
+						//
+						// hero.on('click', function(e) {
+						//   playPause();
+						// });
+						//
+						// document.getElementById('hero-title').addEventListener('click', playPause);
+
+						hero.width(winWidth * 0.6).height(((winWidth * 9) / 16) * 0.6).css('top', (win.height() / 2) - hero.height() / 2);
+
+						$('.watch').on('click', function(e) {
+							e.preventDefault();
+
+							wrapper.addClass('show');
+
+							video.play();
+						});
+
+						$('.close').on('click', function(e) {
+							e.preventDefault();
+
+							wrapper.removeClass('show');
+
+							video.pause();
+
+							video.currentTime = 0;
+						});
+
+						$('.first').on('click', function(e) {
+							// console.log(e.target);
+							if (wrapper.hasClass('show') && !($(e.target).hasClass('watch'))) {
+								wrapper.removeClass('show');
+
+								video.pause();
+
+								video.currentTime = 0;
+							}
+						})
+		}());
 	})(jQuery);
 </script>
 
--- scs/wp-content/themes/reverie-master/page-full.php
+++ scs/wp-content/themes/reverie-master/page-full.php
@@ -4,34 +4,219 @@ Template Name: Full Width
 */
 get_header(); ?>
 
-<!-- Row for main content area -->
-	<div class="small-12 large-12 columns" id="content" role="main">
 
-	<?php /* Start loop */ ?>
-	<?php while (have_posts()) : the_post(); ?>
-		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-			<header>
-				<h1 class="entry-title"><?php the_title(); ?></h1>
-				<?php reverie_entry_meta(); ?>
-			</header>
-			<div class="entry-content">
-				<section id="hero-container">
-					<video data-poster="https://unsplash.imgix.net/uploads%2F14115408840644deb16b0%2F2dc933e3?q=75&fm=jpg&auto=format&s=7f43646e4d26049c6c39890afb2e5ced" id="hero-video" autobuffer>
-						<source src="wp-content/themes/reverie-master/vid/calmthestorm.mp4" >
-						<p>Your Browser is outdated <a href="http://outdatedbrowser.com/" id="video_not_supported">Improve your browsing experience</a>.</p>
-					</video>
-					<h1 id="hero-title">Play</h1>
-				</section>
-				<?php the_content(); ?>
+<!-- Start the main container -->
+<div role="document" class="content-area">
+
+	<header class="full-page section first" >
+		<div class="left">
+			<!-- <h2 class="header">Memories That Last</h2> -->
+			<!-- <p class="subheader">Story Corner Studios is a production studio based in Kansas CIty, Missouri. We specialize in capturing personal stories through interview-based video production.</p> -->
+			<?php $post = get_post(7);
+						echo apply_filters( 'the_content', $post->post_content );
+			?>
+			<button class="button ghost watch">Watch Video</button>
+		</div>
+		<div class="video-wrapper">
+			<video autobuffer controls id="video">
+				<source src="<?php echo get_template_directory_uri(); ?>/vid/calmthestorm.mp4" type="video/mp4"></source>
+			</video>
+			<span class="close">+</span>
+		</div>
+	</header>
+
+	<section class="section second">
+		<!-- <h2 class="header">Pillars of Practice</h2>
+		<ul class="small-block-grid-3 pillars">
+			<li class="pillars-item preservation" data-bottom-top="transform: translate(-100px, 100px); opacity: 0;" data-bottom-bottom="transform: translate(0px, 0px); opacity: 1">
+				<div class="circle hidden"></div>
+				<h3 class="header">Preservation</h3>
+				<p class="desc">Our mission is to make memories last forever.</p>
+			</li>
+			<li class="pillars-item presentation" data-bottom-top="transform: translateY(100px); opacity: 0" data-bottom-bottom="transform: translateY(0px); opacity: 1">
+				<div class="circle hidden"></div>
+				<h3 class="header">Presentation</h3>
+				<p class="desc">We will always make professional work.</p>
+			</li>
+			<li class="pillars-item education" data-bottom-top="transform: translate(100px, 100px); opacity: 0" data-bottom-bottom="transform: translate(0px, 0px); opacity: 1">
+				<div class="circle hidden"></div>
+				<h3 class="header">Education</h3>
+				<p class="desc">We mentor and build up our student associates through training and employment.</p>
+			</li>
+		</ul> -->
+
+		<?php $post = get_post(9);
+					echo apply_filters( 'the_content', $post->post_content );
+		?>
+	</section>
+
+	<section class="section third clearfix">
+		<div class="left">
+			<!-- <h2 class="header">Headline</h2>
+			<p class="subheader">
+				Lorem ipsum dolor sit amet, in vitae gravida ac iaculis. Porttitor vitae pellentesque elementum eu magna, cupidatat nec pellentesque vestibulum sed vitae. Nam natoque, sit tincidunt cras ut, nullam mauris felis suspendisse tellus per mauris, id in diam turpis arcu in.
+			</p>
+			<button class="button ghost">Call to Action</button> -->
+			<?php $post = get_post(12);
+						echo apply_filters( 'the_content', $post->post_content );
+			?>
+		</div>
+		<div class="right hidden">
+			<img src="<?php echo get_template_directory_uri(); ?>/people.png" alt="People" />
+		</div>
+	</section>
+
+	<section class="section fourth">
+		<div class="picture-wrapper">
+			<img class="picture" src="<?php echo get_template_directory_uri(); ?>/img/boat.jpg" alt="Family" />
+		</div>
+		<div class="four-point-wrapper">
+			<ul class="four-point small-block-grid-4">
+				<!-- <li class="point-item">
+					<div>
+						<span class="circle first">1</span>
+						<p>
+							Every person is unique. We tailor our interviews to each individual to best capture their personal Story.
+						</p>
+					</div>
+				</li>
+				<li class="point-item">
+					<div>
+						<span class="circle second">2</span>
+						<p>
+							Our innovative buisness model allows us to  offer professional quality services at family friendly prices.
+						</p>
+					</div>
+				</li>
+				<li class="point-item">
+					<div>
+						<span class="circle third">3</span>
+						<p>
+							We deliver a professional quality video in a two-week turnaround.
+						</p>
+					</div>
+				</li>
+				<li class="point-item">
+					<div>
+						<span class="circle fourth">4</span>
+						<p>
+							We bring a youthful and fun experience in every interview we conduct.
+						</p>
+					</div>
+				</li> -->
+
+				<?php $post = get_post(14);
+							echo apply_filters( 'the_content', $post->post_content );
+				?>
+			</ul>
+		</div>
+		<h2 class="header testimonials">Testimonials</h2>
+		<div class="testimonials-grid-wrapper">
+			<ul class="small-block-grid-3 testimonial-grid">
+				<li class="testimonial-item clearfix">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/test1.png" alt="Terri" class="picture left"/>
+					<!-- <p class="testimonial right">
+						I wish we would've done this sooner! <br /> - Terri Blake
+					</p> -->
+					<?php $post = get_post(21);
+								echo apply_filters( 'the_content', $post->post_content );
+					?>
+				</li>
+				<li class="testimonial-item clearfix">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/test2.png" alt="Cori" class="picture left"/>
+					<!-- <p class="testimonial right">
+						This is the greatest gift a family can recieve. <br /> - Cori Watkins
+					</p> -->
+					<?php $post = get_post(23);
+								echo apply_filters( 'the_content', $post->post_content );
+					?>
+				</li>
+				<li class="testimonial-item clearfix">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/test3.png" alt="Rick" class="picture left"/>
+					<!-- <p class="testimonial right">
+						My family will cherish this forever. <br /> - Rick Bickford
+					</p> -->
+					<?php $post = get_post(25);
+								echo apply_filters( 'the_content', $post->post_content );
+					?>
+				</li>
+			</ul>
+		</div>
+		<div class="pricing-wrapper hidden">
+			<ul class="small-block-grid-3 pricing">
+				<!-- <li class="price-point">
+					<div class="inner-price">
+						<p class="price">$500</p>
+						<ul class="desc-list">
+							<li class="desc">Massa at sodales, morbi mattis lectus, etiam nec nec arcu dictum vestibulum morbi, praesent suscipit.</li>
+							<li class="desc">Massa at sodales, morbi mattis lectus, etiam nec nec arcu dictum vestibulum morbi, praesent suscipit.</li>
+							<li class="desc">Massa at sodales, morbi mattis lectus, etiam nec nec arcu dictum vestibulum morbi, praesent suscipit.</li>
+						</ul>
+						<button class="button purchase">Purchase</button>
+					</div>
+				</li>
+				<li class="price-point">
+					<div class="inner-price">
+						<p class="price">$500</p>
+						<ul class="desc-list">
+							<li class="desc">Massa at sodales, morbi mattis lectus, etiam nec nec arcu dictum vestibulum morbi, praesent suscipit.</li>
+							<li class="desc">Massa at sodales, morbi mattis lectus, etiam nec nec arcu dictum vestibulum morbi, praesent suscipit.</li>
+							<li class="desc">Massa at sodales, morbi mattis lectus, etiam nec nec arcu dictum vestibulum morbi, praesent suscipit.</li>
+						</ul>
+						<button class="button purchase">Purchase</button>
+					</div>
+				</li>
+				<li class="price-point">
+					<div class="inner-price">
+						<p class="price">$500</p>
+						<ul class="desc-list">
+							<li class="desc">Massa at sodales, morbi mattis lectus, etiam nec nec arcu dictum vestibulum morbi, praesent suscipit.</li>
+							<li class="desc">Massa at sodales, morbi mattis lectus, etiam nec nec arcu dictum vestibulum morbi, praesent suscipit.</li>
+							<li class="desc">Massa at sodales, morbi mattis lectus, etiam nec nec arcu dictum vestibulum morbi, praesent suscipit.</li>
+						</ul>
+						<button class="button purchase">Purchase</button>
+					</div>
+				</li> -->
+
+				<?php $post = get_post(18);
+							echo apply_filters( 'the_content', $post->post_content );
+				?>
+			</ul>
+		</div>
+
+		<div class="cities-wrapper">
+			<div class="cities">
+				<!-- <h3 class="header">Cities we cover</h3>
+				<p class="cities-list">
+					Kansas City
+					<span class="pipe">|</span>
+					Olathe, KS
+					<span class="pipe">|</span>
+					Topeka, KS
+					<span class="pipe">|</span>
+					Lawrence, KS
+					<span class="pipe">|</span>
+					Wichita, KS *
+					<span class="pipe">|</span>
+					Colombia, MO *
+					<span class="pipe">|</span>
+					Manhattan, KS *
+					<br />
+				</p>
+				<p class="cost">
+					*Travel cost not included
+				</p> -->
+
+				<?php $post = get_post(27);
+							echo apply_filters( 'the_content', $post->post_content );
+				?>
 			</div>
-			<footer>
-				<?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'reverie'), 'after' => '</p></nav>' )); ?>
-				<p><?php the_tags(); ?></p>
-			</footer>
-			<?php //comments_template(); ?>
-		</article>
-	<?php endwhile; // End the loop ?>
 
-	</div>
+			<div class="cities-img">
+				<img class="img" src="<?php echo get_template_directory_uri(); ?>/img/locations.png" alt="Our Locations" />
+			</div>
+		</div>
+	</section>
+</div>
 
 <?php get_footer(); ?>
